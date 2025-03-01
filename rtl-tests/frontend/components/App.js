5c97bb1d95e86c2bea99b531b3edf34589af4dd9
K1ngHandy <bolds-slicks0g@icloud.com>
2025-03-01 18:48:53 -0500

    Update modified files

--- rtl-tests/frontend/components/App.js
+++ rtl-tests/frontend/components/App.js
@@ -1,16 +1,28 @@
 import React, { useState } from 'react';
 
 export default function App() {
-	const [friends, setFriends] = useState(['Cynthia', 'Fish', 'Alex']);
+	const [friends] = useState(['Cynthia', 'Fish', 'Alex']);
 
 	return (
 		<div data-testid="friends">
 			<h2>Friends App</h2>
 			<div>
-				{friends.map((friend, idx) => {
-					<div key={idx}>My friend {friend}</div>;
-				})}
+				{friends.map((friend, idx) => (
+					<div key={idx}>My friend {friend}</div>
+				))}
 			</div>
+			<form>
+				<img
+					alt="cute cat"
+					src="./cat.jpg"
+					data-testid="catImg"
+				/>
+				<input
+					type="text"
+					placeholder="Type cat name"
+					role="textbox"
+				/>
+			</form>
 		</div>
 	);
 }
--- rtl-tests/frontend/components/__tests__/App.test.js
+++ rtl-tests/frontend/components/__tests__/App.test.js
@@ -3,8 +3,41 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import App from '../App';
 
-test('the container is present', () => {
+test('the container div is present', () => {
 	render(<App />); // render App for test
 	const div = screen.queryByTestId('friends');
 	expect(div).toBeInTheDocument(); // assertion
+});
+
+test('the text "Friends App" renders', () => {
+	render(<App />);
+	const heading = screen.queryByText('Friends App', { exact: false });
+	expect(heading).toBeInTheDocument();
+	expect(heading).toBeVisible();
+});
+
+test('using a loop', () => {
+	render(<App />);
+	['Cynthia', 'Fish', 'Alex'].forEach((name) => {
+		const friend = screen.queryByText(name, { exact: false });
+		expect(friend).toBeVisible();
+	});
+});
+
+test('using queryByAllText', () => {
+	render(<App />);
+	const friends = screen.queryAllByAltText('My Friend', { exact: false });
+	expect(friends).toHaveLength(3);
+});
+
+test('capturing nodes in different ways besides text content', () => {
+	render(<App />);
+	let img = screen.queryByAltText('cute cat');
+	expect(img).toBeInTheDocument();
+	img = screen.queryByTestId('catImg');
+	expect(img).toBeInTheDocument();
+	let input = screen.queryByPlaceholderText('Type cat name');
+	expect(input).toBeInTheDocument();
+	input = screen.queryByRole('textbox');
+	expect(input).toBeInTheDocument();
 });
